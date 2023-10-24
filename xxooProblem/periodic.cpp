59fb2c15e0da99e5feba2db8f8e4cecb9f1f03b1
fernandezdlg <juanfernandezdlg@gmail.com>
2023-10-24 18:47:02 +0200

    periodic done

--- xxooProblem/periodic.cpp
+++ xxooProblem/periodic.cpp
@@ -3,124 +3,221 @@
 #include <algorithm>
 #include <queue>
 #include <bitset>
+#include <array>
 
 // Note: the brute force method yields stack overflow for large N (~80)
-#define constN 16
+#define constN 12
 
 using namespace std;
 
 class Solution
 {
 public:
-    size_t combinationsPeriodicBC(size_t N, bool xx, bool xlast) {
-        // Rules:
-        // the string has {1,0} elements
-        // the string has length N, for N positive even int
-        // 1) The string cannot have any 111 subsequence
-        // 2) The string can have one 11 sequence
-        // 3) The string can have more than one 11 sequence only if the 11 sequences have a 00 in between.
-
-        if (N == 2)
-        {   
-            if (xx) {
-                if (xlast){
-                    return 2; // 11: 00, 01
-                } else {
-                    return 3; // 11..0: 01, 10, 00
-                }
-            } else if (xlast){
-                return 3; // 00..1: 00, 01, 10
-            } else {
-                return 4; // 00 : 00, 01, 10, 11
+    bool noOneRepeated(bitset<constN / 2> const& mask)
+    {
+        // check if there is no 1 repeated in the mask
+        for (size_t i = 0; i < constN / 2 - 1; i++)
+        {
+            if (mask[i] == 1 && mask[i + 1] == 1)
+            {
+                return false;
+            }
+            else
+            {
+                continue;
             }
         }
-
-        if (xx) {
-            if (xlast) {
-                // 11: 00, 01 allowed:
-                return combinationsPeriodicBC(N - 2, false, false) + combinationsPeriodicBC(N - 2, true, true);
-            } else {
-                // 11..0 : 00, 01, 10 allowed:
-                return combinationsPeriodicBC(N - 2, false, false) + combinationsPeriodicBC(N - 2, false, true) + combinationsPeriodicBC(N - 2, true, false);
-            }
-        } else if (xlast) {
-            // 00..1: 10, 00, 01 allowed:
-            return combinationsPeriodicBC(N - 2, true, false) + combinationsPeriodicBC(N - 2, false, false) + combinationsPeriodicBC(N - 2, false, true);
-        } else {
-            // 00..0: 00, 10, 01, 11 allowed:
-            return 2*combinationsPeriodicBC(N - 2, false, false) + combinationsPeriodicBC(N - 2, false, true) + combinationsPeriodicBC(N - 2, true, true);
+        if (mask[0] == 1 && mask[constN / 2 - 1] == 1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
         }
     }
 
-    bool acceptedBitset(bitset<constN> &a){
-        // Rules:
-        // the string has {1,0} elements  
-        // the string has length N, for N positive even int
-        // 1) The string cannot have any 111 subsequence
-        // 2) The string can have one 11 sequence
-        // 3) The string can have more than one 11 sequence only if the 11 sequences have a 00 in between.
-        bool xx = false;
-        bool xlast = false;
-        for (size_t i = 0; i < constN; i++){
-            if (a[i] == 0){
-                if (xx) {
-                    if (xlast){
-                        xlast = false; // 11: 0
-                    } else {
-                        xx = false; // 11..0: 0
-                    }
-                } else if (xlast){
-                    xlast = false; // 00..1: 0
-                } else {
-                    // continue; // 00: 0
-                }
-            } else { // if (a[i] == 1) {
-                if (xx && xlast) { 
-                    return false; // 11: 1 or 11..1: 1
-                } else if (xlast) {
-                    xx = true; // 00..1: 1
-                } else {
-                    xlast = true; // 00: 1
-                }
-            }
-            // } else {
-            //     cout << "Error: bitset has to be binary" << endl;
-            //     return false;
-            // }
-        }    
-        return true;    
-    }
 
-    size_t printAllConfigs(bool print = true){
+    size_t printAllMasksAndConfigs(bool print = true)
+    {
+        bitset<2> b(1);
+        bitset<4> ad(3);
+
         size_t bruteCount = 0;
-        for (size_t i = 0; i < pow(2, constN); i++){
-            bitset<constN> bs(i);
-            if (acceptedBitset(bs)) {
-                if (print) cout << bs << endl;
+        for (size_t i = 0; i < pow(2, constN / 2); i++)
+        {
+            bitset<constN / 2> mask(i);  // mask , e.g. 0000 1111 0000 1111 1111 0000 1111
+            if (noOneRepeated(mask))
+            {
+                if (print)
+                {
+                    cout << "Mask: " << mask << endl;
+                    bitset<constN> bs2(0);
+                    // base:
+                    for (size_t j = 0; j < constN / 2; j++)
+                    {
+                        bs2[2 * j]     = b[0];
+                        bs2[2 * j + 1] = b[1];
+                    }
+
+                    for (size_t j = 0; j < constN/2-1; j++)
+                    {
+                        if (mask[j] == 1)
+                        {
+                            bs2[2 * j]     = ad[0];
+                            bs2[2 * j + 1] = ad[1];
+                            bs2[2 * j + 2] = ad[2];
+                            bs2[2 * j + 3] = ad[3];
+                        }
+                    }
+                    size_t lastj = constN/2 - 1;
+                    if (mask[lastj] == 1) {
+                        bs2[2*lastj    ] = ad[0];
+                        bs2[2*lastj + 1] = ad[1];
+                        bs2[0]           = ad[2];
+                        bs2[1]           = ad[3];
+                    }
+                    cout << "Config: " << bs2 << endl;
+                }
                 bruteCount++;
-            }    
+            }
         }
         return bruteCount;
     }
 
+    size_t countPeriodicNoRepeatedOne(){
+        const size_t N = constN/2;
+        size_t a[N], b[N], c[N], d[N];
+        a[0] = b[0] = c[0] = d[0] = 0; // undefined
+        a[1] = b[1] = c[1] = 1; // 0..0, 0..1, 1..0, 1..1 and no repeated
+        d[1] = 0; // 11 repeated
+        cout << "{" << (4) << "," << (a[1] + b[1] + c[1]) << "},";
+        for (size_t i = 2; i < N; i++) {
+            a[i] = a[i-1] + b[i-1];
+            b[i] = a[i-1];
+            c[i] = c[i-1] + d[i-1];
+            d[i] = c[i-1];
+            cout << "{" << (2*(i+1)) << "," << (a[i] + b[i] + c[i]) << "},";
+        }
+        // cout << "{" << (2*(N-1)) << "," << (a[N] + b[N] + c[N]) << "},";
+        return (a[N-1] + b[N-1] + c[N-1]);
+    }
+
+    size_t printAllMasks(bool print = true)
+    {
+        size_t bruteCount = 0;
+        for (size_t i = 0; i < pow(2, constN / 2); i++)
+        {
+            bitset<constN / 2> mask(i);  // mask , e.g. 0000 1111 0000 1111 1111 0000 1111
+            if (noOneRepeated(mask))
+            {
+                if (print)
+                {
+                    cout << mask << endl;
+                }
+                bruteCount++;
+            }
+        }
+        return bruteCount;
+    }
+
+    size_t printAllConfigs(bool print = true)
+    {
+        bitset<2> b(1);
+        bitset<4> ad(3);
+
+        size_t bruteCount = 0;
+        for (size_t i = 0; i < pow(2, constN / 2); i++)
+        {
+            bitset<constN / 2> mask(i);  // mask , e.g. 0000 1111 0000 1111 1111 0000 1111
+            if (noOneRepeated(mask))
+            {
+                if (print)
+                {
+                    bitset<constN> bs2(0);
+                    // base:
+                    for (size_t j = 0; j < constN / 2; j++)
+                    {
+                        bs2[2 * j]     = b[0];
+                        bs2[2 * j + 1] = b[1];
+                    }
+
+                    for (size_t j = 0; j < constN/2-1; j++)
+                    {
+                        if (mask[j] == 1)
+                        {
+                            bs2[2 * j]     = ad[0];
+                            bs2[2 * j + 1] = ad[1];
+                            bs2[2 * j + 2] = ad[2];
+                            bs2[2 * j + 3] = ad[3];
+                        }
+                    }
+                    size_t lastj = constN/2 - 1;
+                    if (mask[lastj] == 1) {
+                        bs2[2*lastj    ] = ad[0];
+                        bs2[2*lastj + 1] = ad[1];
+                        bs2[0]           = ad[2];
+                        bs2[1]           = ad[3];
+                    }
+                    cout << "Config: " << bs2 << endl;
+                }
+                bruteCount++;
+            }
+        }
+        return bruteCount;
+    }
 };
 
 
-int main(){
+int main()
+{
     // Define size of chain (must be even positive int)
     Solution solution;
 
-    size_t const testN = constN; // testN >= 2
+    size_t const testN = constN;  // testN >= 2
 
     static_assert(testN % 2 == 0, "N must be even");
     static_assert(testN >= 2, "N >= 2");
 
-    bool print = true; // print all configs if true
-    if (print) cout << "All configs printed:" << endl;
-    size_t bruteCount = solution.printAllConfigs(print);
-    cout << "Brute force count: " << bruteCount << endl;
+    bool print = true;  // print all configs if true
+    size_t bruteCount = 0;
 
-    size_t result = solution.combinationsPeriodicBC(testN, false, false);
-    cout << "Result: " << result << endl;
+    // Tests for noOneRepeated
+    cout << "Tests for noOneRepeated:" << endl;
+    bitset<testN/2> mask(0);
+    cout << "mask: " << mask << endl;
+    cout << "noOneRepeated: " << solution.noOneRepeated(mask) << endl;
+    mask = bitset<testN/2>(3);
+    cout << "mask: " << mask << endl;
+    cout << "noOneRepeated: " << solution.noOneRepeated(mask) << endl;
+    mask = bitset<testN/2>(5);
+    cout << "mask: " << mask << endl;
+    cout << "noOneRepeated: " << solution.noOneRepeated(mask) << endl << endl;
+
+    // Tests for printAllMasksAndConfigs
+    cout << "Tests for printAllMasksAndConfigs:" << endl;
+    if (print)
+        cout << "All masks and configs printed:" << endl;
+    bruteCount = solution.printAllMasksAndConfigs(print);
+    cout << "Brute force count: " << bruteCount << endl << endl;
+
+    // Tests for countPeriodicNoRepeatedOne
+    cout << "Tests for countPeriodicNoRepeatedOne:" << endl;
+    auto result = solution.countPeriodicNoRepeatedOne();
+    cout << "\nResult: " << result << endl << endl;
+
+    // Tests for printAllMasks
+    cout << "Tests for printAllMasks:" << endl;
+    if (print)
+        cout << "All masks printed:" << endl;
+    bruteCount = solution.printAllMasks(print);
+    cout << "Brute force count: " << bruteCount << endl << endl;
+
+    // Tests for printAllConfigs
+    cout << "Tests for printAllConfigs:" << endl;
+    if (print) cout << "All configs printed:" << endl;
+    bruteCount = solution.printAllConfigs();
+    cout << "Brute force count: " << bruteCount << endl << endl;
+
     return 0;
 }
